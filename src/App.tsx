c2f86365be4fbd447bbede9e222097a7a8d7698c
Anton Golubev <AnDGolubev@srt.beeline.ru>
2024-12-28 01:25:00 +0400

    реализовано переключение тем

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import { Suspense } from "react";
+import { Suspense, useContext, useState } from "react";
 import { Route, Routes } from "react-router-dom"
 import { Link } from "react-router-dom";
 import MainPageAsync from "./pages/MainPage/MainPage.async";
 import AboutPageAsync from "./pages/AboutPage/AboutPage.async";
-import './index.scss';
-
+import './styles/index.scss';
+import { useTheme } from "./theme/useTheme";
 
 export const App = () => {
+	const { theme, toggleTheme } = useTheme();
+
 	return (
-		<div className="app">
+		<div className={`app ${theme}`}>
+			<button onClick={toggleTheme}>TOGGLE</button>
 			<Link to={'/'}>Главная</Link>
 			<Link to={'/about'}>О сайте</Link>
 			<Suspense fallback={<div>loading...</div>}>
